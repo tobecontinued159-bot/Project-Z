74ee79c00c2fd52a6a9c6d0f4e16f3561c1ef294
Joeza007 <tobecontinued159@gmail.com>
2026-08-22 16:32:34 +0700

    weapon

---
+++ Assets/Code/NetworkPlayer/PlayerWeapon.cs
@@ -0,0 +1,35 @@
+using Fusion;
+using UnityEngine;
+
+public class PlayerWeapon : NetworkBehaviour
+{
+    [SerializeField] private float weaponRange = 50f;
+    [SerializeField] private float muzzleHeight = 0.5f;
+
+    private void Update()
+    {
+        if (HasStateAuthority == false)
+        {
+            return;
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RPC_Fire();
+        }
+    }
+
+    [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
+    private void RPC_Fire()
+    {
+        Vector3 muzzlePosition = transform.position + Vector3.up * muzzleHeight;
+        Vector3 fireDirection = transform.forward;
+
+        Debug.DrawRay(muzzlePosition, fireDirection * weaponRange, Color.red, 1f);
+
+        if (Physics.Raycast(muzzlePosition, fireDirection, out RaycastHit hit, weaponRange))
+        {
+            Debug.Log($"Hit: {hit.collider.gameObject.name}");
+        }
+    }
+}
